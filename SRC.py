4feb1d942db2c2b282449f3621c3dc734fc67f64
Musashi <chrismusashi85@gmail.com>
2019-02-26 11:53:44 +0900

    Update SRC.py

--- SRC.py
+++ SRC.py
@@ -3,16 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 '''
-44.1kHzのSIN波を48kHzのSIN波にSRCするプログラム
-
+Fs_in[Hz]のSIN波をFs_out[Hz]のSIN波にSRCするプログラム
+Fs_in <= Fs_out が条件
 '''
-LW = 32 # 畳み込みする関数の長さ（-LW*π/2からLW*π/2まで）
+N = Fs_in/100+1 # 入力信号のサンプル数
+LW = 32 # sinc()を畳み込みする長さ（-LW*π/2からLW*π/2まで）
 pi = math.pi
 
-Fs_in = 44100
-Fs_out = 48000
+Fs_in = 44100 # ここを任意（ただしFs_in <= Fs_out）で変える。入力信号生成部もあわせて修正する。
+Fs_out = 48000 # ここを任意で変える
 
-N = int(Fs_out/200) # Number of input samples　入力信号のサンプル数
+N_out = int(Fs_out/100) # SRC後信号のサンプル数（1kHzのSin波 10周期分）
 
 def hamming(x):
     return 0.54-0.46*math.cos(x)
@@ -30,32 +31,19 @@ def convolution(x, m):
     sum = 0
     (dn, n) = math.modf(m * Fs_in/Fs_out)
     for k in range(int(-LW/2), int(LW/2)):
-        if (n+k>=0) and (n+k<N):
+        if (int(n+k)>=0) and (int(n+k)<N):
             sum = sum + x[int(n+k)] * windowed_sinc(k-dn)
     return sum
     
 y = [] # output
-x0 = np.arange(0, 10*2*pi, 2*pi/44.1) # 44.1kHzで10周期分の1khzのSIN波
+# 入力信号生成部
+x0 = np.arange(0, 10*2*pi, 2*pi/44.1) # 44.1kHzで10周期分の1khzのSIN波（SRCの入力信号）
 x1 = np.sin(x0)
 x2 = x1.tolist()
-#x2.extend([0] * int(LW/2))
 
-'''
-x = np.arange(-LW/2, LW/2, 1/10)
+x = np.arange(N_out)
 
-y2 = []
-
-for x1 in x:
-    y1.append(windowed_sinc(x1))
-    y2.append(sinc(x1))
-
-plt.plot(x, y1, label = "y = windowed_sinc(x)")
-plt.plot(x, y2, label = "y = sinc(x)")
-'''
-x = np.arange(N)
-x_221 = np.arange(0, 221)
-
-for m1 in range(0, N):
+for m1 in range(0, N_out):
     y.append(convolution(x2, m1))
 
 # input signal
